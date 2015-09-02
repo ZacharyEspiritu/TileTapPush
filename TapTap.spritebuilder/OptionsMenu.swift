240cf5a8692744d9c110fe38e48ec7022870626e
Zachary Espiritu <zespiritu17@regis.org>
2015-09-02 17:11:14 -0400

    Changed copyright notices

--- TapTap.spritebuilder/OptionsMenu.swift
+++ TapTap.spritebuilder/OptionsMenu.swift
@@ -3,7 +3,7 @@
 //  TapTap
 //
 //  Created by Zachary Espiritu on 7/3/15.
-//  Copyright (c) 2015 Apportable. All rights reserved.
+//  Copyright (c) 2015 Zachary Espiritu. All rights reserved.
 //
 
 import Foundation
--- TapTap.spritebuilder/SinglePlayer.swift
+++ TapTap.spritebuilder/SinglePlayer.swift
@@ -3,7 +3,7 @@
 //  TapTap
 //
 //  Created by Zachary Espiritu on 6/30/15.
-//  Copyright (c) 2015 Apportable. All rights reserved.
+//  Copyright (c) 2015 Zachary Espiritu. All rights reserved.
 //
 
 import Foundation
